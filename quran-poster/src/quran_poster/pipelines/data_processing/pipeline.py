b6e1d817490231c218b9a9eff5e339c90f3853b9
Muneeb Alam <muneeb.alam@gmail.com>
2025-04-27 12:52:12 -0400

    second commit

--- quran-poster/src/quran_poster/pipelines/data_processing/pipeline.py
+++ quran-poster/src/quran_poster/pipelines/data_processing/pipeline.py
@@ -24,5 +24,33 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs="model_input_table",
                 name="create_model_input_table_node",
             ),
+            # translate desired dimensions into pixels
+            node(
+                func=translate_dimensions_to_pixels,
+                inputs = ["params:poster_width_in", "params:poster_height_in"],
+                outputs = ["poster_width_px", "poster_height_px"],
+            ),
+            # create blank canvas
+            node(
+                func=create_blank_canvas,
+                inputs = ["poster_width_px", "poster_height_px"],
+                outputs = "blank_canvas",
+            ),
+            # add background image
+            node(
+                func=add_background_image,
+                inputs = ["blank_canvas", "params:background_image_path"],
+                outputs = "background_image_canvas",
+            ),
+            # add desired user quran text and bounding box into font size
+            node(
+                func=add_arabic_text,
+                inputs = ["background_image_canvas", "params:text"],
+                outputs = "arabic_text_canvas",
+            ),
+            # add arabic
+            # repeat for english
+            # Add english
+            # save
         ]
     )
